dc49828f04a3d3f527924f9f96dd8b653a339207
JaeYeon Kim <105400221+Sanic00@users.noreply.github.com>
2022-09-13 18:21:29 +0900

    Add files via upload

---
+++ spring_workspace/TeamProject/src/main/java/com/Team/sample/Chef.java
@@ -0,0 +1,13 @@
+package com.Team.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data //lombok을 통해 getter/setter/toString 자동 형성 됨 
+public class Chef {
+
+	
+	
+}
